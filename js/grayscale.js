5615a0d5fb7d09e68e6b5e9f91e4fc19038ba2b5
Jorge Arias <mail@jorgearias.cl>
2015-10-14 22:32:41 -0300

    Refactor $anchor.blur() location

--- js/grayscale.js
+++ js/grayscale.js
@@ -17,10 +17,10 @@ $(window).scroll(function() {
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
-        $anchor.blur();
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+        }, 1500, 'easeInOutExpo'
+        , function(){ $anchor.blur(); });
         event.preventDefault();
     });
 });
