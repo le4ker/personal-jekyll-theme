eed5a5e8e498adb92a5bc44562313a7135c89438
Panos Sakkos <panos.sakkos@gmail.com>
2024-04-24 23:25:59 +0300

    load disqus comments js only in pages with comments

--- bin/new_post.rb
+++ bin/new_post.rb
@@ -10,7 +10,7 @@ post = ARGV[0]
 
 File.open("#{POSTS_DIR}/#{Date.today.strftime('%Y-%m-%d-')}#{post}.md", 'w') do |f|
   f.write(
-    "---\nlayout: post\nsection-type: post\ntitle: Title\ncategory: Category\ntags: [ \"tag1\", \"tag2\" ]\n---"
+    "---\nlayout: post\nsection-type: post\nhas-comments: true\ntitle: Title\ncategory: Category\ntags: [ \"tag1\", \"tag2\" ]\n---"
   )
 end
 
